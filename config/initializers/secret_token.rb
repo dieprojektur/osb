c8f3a964affe9d7734c75f0584d94fb3e3acbc09
Anton Bangratz <anton.bangratz@gmail.com>
2010-12-10 13:41:50 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Osb::Application.config.secret_token = '30d9fcd918b27eb12776eb1a7a1d1fc43b0bb14cab1a524a110176b8fdc72f109a87dc1ecd6660d7e7de08dafeff040c0f270324e79efb41874c0ef95c4c9523'
